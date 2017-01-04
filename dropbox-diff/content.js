687b24e0e3ecac5120e61d9a9ab78b850e384a36
Victor Shih <victor.shih@gmail.com>
2017-01-04 02:41:35 -0800

    Get inline diff working again for latest Dropbox version.

--- dropbox-diff/content.js
+++ dropbox-diff/content.js
@@ -1,46 +1,202 @@
 
-// ===== Globals
+'use strict';
+
+// jQuery alias.
+(($) => {
 
 
-// Column index constants, after diff column added; 0-base indexed.
-var C = {
-	PREVIEW: 1,
-	DIFF: 2,
-	CHANGED: 5
-};
-
-// Reference to revision table.
-var $REV_TABLE = $('table.filebrowser');
-
-// Inline diff-viewer references.
-var $INDIFF_VIEW;
-var PREVIEW_TOP;
-
-// Retrieved file text cache.
-var TEXT_CACHE = {};
+// ===== Globals/bookmarks.
 
 
-// ===== Functions
+let $WINDOW;
+let $BODY;
+
+// Root node of the revisions-list markup.
+let $REV_DIV;
+
+// Reference for injecting page content.
+let $PAGE_CONTENT;
+
+// Inline diff-viewer references/values.
+let $INDIFF_VIEW;
+let $FILE_PREVIEW_MODAL;
+
+let PREVIEW_TOP_PX;
+
+// Extracted revision information, keyed by sjid.
+let REV_MAP;
+
+// Cache for retrieved file text.
+let TEXT_CACHE = {};
 
 
-function get_file_info(l_or_r) {
-	var radio = $REV_TABLE.find('input[name=diff_' + l_or_r + ']').filter(':checked');
+// ===== Functions - markup injection and extraction.
 
-	if (radio.length != 1) { return null }
 
-	var row = radio.closest('tr');
+function initBookmarks() {
+	$WINDOW				= $(window);
+	$BODY					= $(document.body);
+	$PAGE_CONTENT	= $('#page-content');
+}
+
+
+// Register a callback to fire when React is done mounting.
+// There's probably a proper way to do this using onComponentDidMount() but I don't know it.
+function registerReactReadyListener(target, callback) {
+	let observer = new MutationObserver(mutations => {
+		// A single event seems like enough for the whole DOM to be complete.
+
+		callback();
+
+		observer.disconnect();
+	});
+
+	observer.observe(target, {
+		childList: true,
+		subtree: true
+	});
+}
+
+
+function injectInlineDiffMarkup() {
+	// Add inline-diff overlay.  This markup was lifted from an older version of Dropbox's photo-preview page.
+	let close_icon_src = chrome.extension.getURL('icon_spacer-vflN3BYt2.gif');
+
+	$BODY.append(`
+		<div id="file-preview-modal" style="display: none">
+			<div class="modal-preview-content">
+				<div class="preview">
+					<table class="preview-container-parent"><tbody>
+						<tr>
+							<td class="preview-container" style="cursor: default">
+								<div class="preview-content">
+									<div class="content-item">
+										<div id="indiff-view"></div>
+									</div>
+								</div>
+							</td>
+						</tr>
+					</tbody></table>
+				</div>
+			</div>
+			<div class="header" style="cursor: default">
+				<a href="#" class="close lightbox-not-important">
+					<img class="sprite sprite_web s_web_control_close" alt="Close"
+						src="${close_icon_src}">
+				</a>
+			</div>
+		</div>
+	`);
+	$FILE_PREVIEW_MODAL = $('#file-preview-modal');
+	PREVIEW_TOP_PX = parseInt($FILE_PREVIEW_MODAL.find('.preview').css('top').replace(/px/, ''));
+	$INDIFF_VIEW = $('#indiff-view');
+
+	// Handle close, ESC.
+	$FILE_PREVIEW_MODAL.find('.close').click(hideInlineDiff);
+	$(document).keyup((ev) => { if (ev.keyCode == 27) hideInlineDiff() });
+
+	$WINDOW.resize(windowOnResize);
+
+
+	$PAGE_CONTENT
+		.on('click', '.diff-sel', (ev) => {
+			// Prevent default preview pop-up for injected elements.
+			ev.stopPropagation();
+		})
+		// Add diff-selection handlers.
+		.on('click', '.diff-sel input[type="radio"]', refreshDiffButtons)
+	;
+}
+
+
+// Revision info is stored as a JSON object within a script tag.
+// TODO Not sure what needs to happen when the user clicks "Load older versions".
+function extractRevInfo() {
+	// Assumptions:
+	//	- JSON is not minified.
+	//	- "revisions" array contains no "]" embedded characters.
+	let re = /"revisions": (\[[^\]]+\])/;
+
+	let json = null;
+
+	$.each(document.scripts, (i, script) => {
+		let match = script.innerText.match(re);
+
+		if (match) {
+			json = match[1];
+			return false;
+		}
+	});
+
+	if (!json) {
+		return alert('DropboxDiff failed to extract revision information.');
+	}
+
+	let revisions = JSON.parse(json);
+
+	REV_MAP = {};
+
+	// Example direct_blockserver_link value:
+	//	https://www.dropbox.com/pri/get/{path}/{file}.{ext}?_subject_uid=1252292&w=AAA...
+	revisions.forEach(r => {
+		REV_MAP[r.id] = r.preview_info.direct_blockserver_link;
+	});
+}
+
+
+function initBookmarksMounted() {
+	$REV_DIV = $('.file-revisions-page__content');
+}
+
+
+// Insert diff buttons and handlers.
+function injectDiffButtons() {
+	$('#inner-page-header').prepend(`
+		<div style="display: inline-block; margin-left: 189px">
+			<button id="exdiff" class="diff-button freshbutton-lightblue" disabled>Diff</button>
+			<button id="indiff" class="diff-button freshbutton-lightblue" disabled>Inline</button>
+		</div>
+	`);
+
+	// Diff button handlers.
+	$('#indiff').click(() => { diffOnClick('in') });
+	$('#exdiff').click(() => { diffOnClick('ex') });
+}
+
+function injectRadioButtons() {
+	$REV_DIV.find('.file-revisions__row_fake_wrapper_col').prepend(`
+		<div class="file-revisions__row__col diff-sel" style="text-align: center; width: 65px">
+			<input type="radio" name="diff-l" title="left side"/>
+			<input type="radio" name="diff-r" title="right side"/>
+		</div>
+	`);
+}
+
+
+// ===== Functions - file retrieval.
+
+
+function getFileInfo(l_or_r) {
+	let $radio = $REV_DIV.find('input[name=diff-' + l_or_r + ']').filter(':checked');
+
+	if ($radio.length != 1) { return null }
+
+	let $row = $radio.closest('li');
+	let sjid = $row.data('identity').split('_')[1];
 
 	return {
-		url:			row.find('td:nth-child(' + (C.PREVIEW + 1) + ') > a').attr('href'),
-		changed:	row.find('td:nth-child(' + (C.CHANGED + 1) + ')').text()
+		sjid:			sjid,
+		url:			REV_MAP[sjid],
+		// Strip whitespace and dot.
+		changed:	$row.find('.file-revisions__text--time').text().trim().substr(2)
 	};
 }
 
 
 // Retrieve info of files to diff:  {is_valid, left, right}.
-function get_file_infos() {
-	var left = get_file_info('l');
-	var right = get_file_info('r');
+function getFileInfos() {
+	let left = getFileInfo('l');
+	let right = getFileInfo('r');
 
 	return {
 		// Are they both selected, and different?
@@ -51,51 +207,67 @@ function get_file_infos() {
 }
 
 
+function sanitizeDropboxDate(d) {
+	return d.replace(/[\/:]/g, '.').replace(/ ([AP]M)/, '$1').replace(/^\s+|\s+$/g, '').replace(/\s+\(.+\)/, '').toLowerCase();
+}
+
+
+function computeFileName(url, sjid, changed) {
+	// Extract filename from URL.
+	let basename = url.substr(url.lastIndexOf('/') + 1);
+	basename = decodeURIComponent(basename.substr(0, basename.indexOf('?')));
+
+	let dot = basename.lastIndexOf('.');
+	let ext = dot == -1 ? '' : basename.substr(dot);
+	let base = basename.substr(0, basename.length - ext.length);
+
+	// Append revision to ensure uniqueness.
+	// Keep extension, in case diff tool can use it.
+	return sanitizeDropboxDate(changed) + ` ${base} ${sjid}${ext}`;
+}
+
+
+function getFileText(url) {
+	if (url in TEXT_CACHE) return $.Deferred().resolve(TEXT_CACHE[url]);
+
+	return $.get(url).then(text => {
+		TEXT_CACHE[url] = text;
+		return text;
+	});
+}
+
+
+// ===== Functions - UI, event handling.
+
+
 // Update "enable" status of diff buttons.
-function refresh_diff_button() {
-	(get_file_infos().is_valid ? $.fn.removeClass : $.fn.addClass).call($('.diff_button'), 'grayed');
+function refreshDiffButtons() {
+	let left = $('[name="diff-l"]:checked');
+	let right = $('[name="diff-r"]:checked');
+
+	$('.diff-button').prop('disabled', !(left.length && right.length && left.parent().get(0) != right.parent().get(0)));
 }
 
 
-// Handle diff selection changes.
-function diff_sel_changed(ev) {
-	// Store row content for display on other pages.
-	// Uncheck the restore button first, and check the targeted button.
-	var name = 'name="' + ev.target.name + '"';
-
-	localStorage[ev.target.name] = $(ev.target).closest('tr').html()
-		.replace('checked="checked"', '').replace(name, name + ' checked="checked"');
-
-	// Store current path name for validation.
-	localStorage.pathname = document.location.pathname;
-
-	refresh_diff_button();
-}
-
-
-// Diff button handlers.
-function indiff_onclick() { diff_onclick('in') }
-function exdiff_onclick() { diff_onclick('ex') }
-
-function diff_onclick(in_or_ex) {
+function diffOnClick(in_or_ex) {
 	// Retrieve left and right previews.
-	var files = get_file_infos();
+	let files = getFileInfos();
 
 	if (!files.is_valid) { return }
 
 
 	// It's legit.
-	var left_name = compute_file_name(files.left.url, files.left.changed);
-	var right_name = compute_file_name(files.right.url, files.right.changed);
+	let left_name = computeFileName(files.left.url, files.left.sjid, files.left.changed);
+	let right_name = computeFileName(files.right.url, files.right.sjid, files.right.changed);
 
 	// Retrieve text for both files from cache or ajax.
 	$.when(
-		get_file_text(files.left.url),
-		get_file_text(files.right.url)
-	).done(function (left_text, right_text) {
+		getFileText(files.left.url),
+		getFileText(files.right.url)
+	).done((left_text, right_text) => {
 		if (in_or_ex == 'in') {
-			var left_lines = difflib.stringAsLines(left_text);
-			var right_lines = difflib.stringAsLines(right_text);
+			let left_lines = difflib.stringAsLines(left_text);
+			let right_lines = difflib.stringAsLines(right_text);
 
 			$INDIFF_VIEW
 				.empty()
@@ -111,11 +283,11 @@ function diff_onclick(in_or_ex) {
 					})
 				);
 
-			show_indiff();
+			showInlineDiff();
 		}
 		else {
 			// External diff.
-			var ex_data = {
+			let ex_data = {
 				left: {
 					name: left_name,
 					text: left_text
@@ -126,12 +298,12 @@ function diff_onclick(in_or_ex) {
 				}
 			};
 
-			var body = $(document.body).addClass('progress');
-			var cleanup = function () { body.removeClass('progress') };
+			$BODY.addClass('progress');
+			let cleanup = () => { $BODY.removeClass('progress') };
 
-			var tries = 3;
+			let tries = 3;
 
-			var callback = function (response) {
+			let callback = (response) => {
 				switch (response) {
 					case '':
 						// Success.
@@ -144,7 +316,7 @@ function diff_onclick(in_or_ex) {
 						// what seems like a Chrome bug.
 						--tries;
 						if (tries) {
-							setTimeout(function () { chrome.runtime.sendMessage(ex_data, callback) }, 500);
+							setTimeout(() => { chrome.runtime.sendMessage(ex_data, callback) }, 500);
 							break;
 						}
 						// else fall through.
@@ -167,187 +339,45 @@ function diff_onclick(in_or_ex) {
 }
 
 
-function show_indiff() {
-	$('#file-preview-modal').show();
+function showInlineDiff() {
+	$FILE_PREVIEW_MODAL.show();
 	$('html').css({overflowY: 'hidden'});
-	$(window).resize();	// trigger diff modal height adjustment.
+	$WINDOW.resize();	// trigger diff modal height adjustment.
 }
 
-function hide_indiff() {
-	$('#file-preview-modal').hide();
+function hideInlineDiff() {
+	$FILE_PREVIEW_MODAL.hide();
 	$('html').css({overflowY: ''});
 }
 
 
 // Dynamically calculate diff-modal viewport (simulate "max-height: 100%").
-function window_onresize() {
-	if ($INDIFF_VIEW.is(':visible')) $INDIFF_VIEW.css({maxHeight: $(window).height() - PREVIEW_TOP});
+function windowOnResize() {
+	if ($INDIFF_VIEW.is(':visible')) $INDIFF_VIEW.css({maxHeight: $WINDOW.height() - PREVIEW_TOP_PX});
 }
 
 
-function insert_row(tbodies, l_or_r) {
-	var content = localStorage[l_or_r];
+// ===== Main.
 
-	if (!content) { return }
 
-	var sjid = extract_sjid(content);
+// Stuff that can be initialized on document.ready.
+$(() => {
+	initBookmarks();
 
-	var rows = tbodies[1].rows;
-	var row_count = rows.length - 1; // account for empty row.
+	registerReactReadyListener($PAGE_CONTENT[0], onReactReady);
+	injectInlineDiffMarkup();
+	extractRevInfo();
+});
 
-	var hi_sjid = extract_sjid(rows[0].innerHTML);
-	var lo_sjid = extract_sjid(rows[row_count - 1].innerHTML);
 
-	var dest;
+// Stuff that must be performed after React components are loaded.
+function onReactReady() {
+	initBookmarksMounted();
 
-	if (sjid > hi_sjid) {
-		dest = $(tbodies[0]);
-	}
-	else if (sjid < lo_sjid) {
-		dest = $(tbodies[2]);
-	}
-	else {
-		// The revision is on this page; clear it from storage.
-		delete localStorage[l_or_r];
-		return;
-	}
-
-	content = $('<tr/>', {'class': 'tr-added'}).append(content);
-
-	// Insertion sort.
-	dest.find('> tr').each(function (i, tr) {
-		var row_sjid = extract_sjid(tr.innerHTML);
-
-		if (sjid > row_sjid) {
-			$(tr).before(content);
-			content = null;
-			return false;
-		}
-	});
-
-	if (content) { dest.append(content) }
+	injectDiffButtons();
+	injectRadioButtons();
 }
 
-
-// Extract revision from HTML content or URLs.
-function extract_sjid(html) {
-	return /(\?|&(amp;)?)sjid=([0-9]+)/.exec(html)[3];
-}
-
-
-function sanitize_dropbox_date(d) {
-	return d.replace(/[\/:]/g, '.').replace(/ ([AP]M)/, '$1').replace(/^\s+|\s+$/g, '').replace(/\s+\(.+\)/, '').toLowerCase();
-}
-
-
-function compute_file_name(url, changed) {
-	// Extract filename and revision ("sjid" parameter).
-	var rev = extract_sjid(url);
-
-	var basename = url.substr(url.lastIndexOf('/') + 1);
-	basename = decodeURIComponent(basename.substr(0, basename.indexOf('?')));
-
-	var dot = basename.lastIndexOf('.');
-	var ext = dot == -1 ? '' : basename.substr(dot);
-	var base = basename.substr(0, basename.length - ext.length);
-
-	// Append revision to ensure uniqueness.
-	// Keep extension, in case diff tool can use it.
-	return sanitize_dropbox_date(changed) + ' ' + base + ' ' + rev + ext;
-}
-
-
-function get_file_text(url) {
-	if (url in TEXT_CACHE) return $.Deferred().resolve(TEXT_CACHE[url]);
-
-	return $.get(url).then(function (text) {
-		TEXT_CACHE[url] = text;
-		return text;
-	});
-}
-
-
-// ===== Main
-
-
-(function ($) {
-
-	var tbody = $REV_TABLE.find('> tbody');
-
-	// If there's only one revision, don't bother modifying table.
-	if (tbody.find('> tr:has(td > a)').length == 1) { return }
-
-	// Insert diff column for each row.
-	tbody.find('> tr > td:nth-child(' + (C.PREVIEW + 1) + ')').after(
-		'<td>' +
-			'<div class="diff_sel">' +
-				'<input type="radio" name="diff_l" title="left side"/>' +
-				'<input type="radio" name="diff_r" title="right side"/>' +
-			'</div>' +
-		'</td>'
-	);
-
-	// Add before- and after- tbodies.
-	tbody.before($('<tbody/>')).after($('<tbody/>'));
-
-	var tbodies = $REV_TABLE.find('> tbody');
-
-	// Check if stored row content matches current page.
-	if (localStorage.pathname == document.location.pathname) {
-		insert_row(tbodies, 'diff_l');
-		insert_row(tbodies, 'diff_r');
-
-		// Add margin rows, if necessary.
-		$([tbodies[0], tbodies[2]]).filter(':parent').append($('<tr><td colspan="8">&nbsp;</td></tr>'));
-	}
-
-	// Add handlers.
-	tbodies.find('.diff_sel > input').click(diff_sel_changed);
-
-	// Insert diff buttons and handlers.
-	$REV_TABLE.next('div').prepend(
-		'<input id="indiff_button" type="button" value="Inline" class="diff_button freshbutton-lightblue grayed"/>' +
-		'<input id="exdiff_button" type="button" value="Diff" class="diff_button freshbutton-lightblue grayed"/>'
-	);
-
-	$('#indiff_button').click(indiff_onclick);
-	$('#exdiff_button').click(exdiff_onclick);
-
-	// Add inline-diff overlay.  This was lifted from Dropbox's photo-preview page.
-	$(document.body).append(
-		'<div id="file-preview-modal" style="display: none">' +
-			'<div class="modal-preview-content">' +
-				'<div class="preview">' +
-					'<table class="preview-container-parent"><tbody>' +
-						'<tr>' +
-							'<td class="preview-container" style="cursor: default">' +
-								'<div class="preview-content">' +
-									'<div class="content-item">' +
-										'<div id="indiff-view"></div>' +
-									'</div>' +
-								'</div>' +
-							'</td>' +
-						'</tr>' +
-					'</tbody></table>' +
-				'</div>' +
-			'</div>' +
-			'<div class="header" style="cursor: default">' +
-				'<a href="#" class="close lightbox-not-important">' +
-					'<img src="https://dt8kf6553cww8.cloudfront.net/static/images/icons/icon_spacer-vflN3BYt2.gif" class="sprite sprite_web s_web_lightbox_close">' +
-				'</a>' +
-			'</div>' +
-		'</div>'
-	);
-	$INDIFF_VIEW = $('#indiff-view');
-	PREVIEW_TOP = $('#file-preview-modal .preview').css('top').replace(/px/, '');
-
-	// Handle close, ESC.
-	$('#file-preview-modal .close').click(hide_indiff);
-	$(document).keyup(function (ev) { if (ev.keyCode == 27) hide_indiff() });
-
-	$(window).resize(window_onresize);
-
-	refresh_diff_button();
 
 })(jQuery);
 
